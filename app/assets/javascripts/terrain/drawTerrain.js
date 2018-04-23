de18da6acef8431add470cfa3d13b4ffe2b221ad
= <chipwibb@gmail.com>
2018-04-23 16:14:08 -0700

    cursor

--- app/assets/javascripts/terrain/drawTerrain.js
+++ app/assets/javascripts/terrain/drawTerrain.js
@@ -1,6 +1,6 @@
 var Terrain = {
 	draw: function () {
-		ctx.fillStyle = 'rgba(0, 0, 0, 0.2)'; //use alpha element for motion blur (1 = none, 0 = all)
+		ctx.fillStyle = 'rgba(0, 0, 0, 0.4)'; //use alpha element for motion blur (1 = none, 0 = all)
 		ctx.beginPath();
 
 		ctx.rect(0, 0, canvas.width, canvas.height);
