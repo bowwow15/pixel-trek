65b9bbe4faa88c7c49431270531ff87a0e462cd7
= <chipwibb@gmail.com>
2018-05-14 16:43:43 -0700

    white square represents server players

--- app/assets/javascripts/webSockets/server.js
+++ app/assets/javascripts/webSockets/server.js
@@ -7,10 +7,13 @@ var Server = {
 
 	drawPlayers: function () {
 		this.players.forEach(function (element, index) {
+			let aug_x = element.x + View.x;
+			let aug_y = element.y + View.y;
+
 			ctx.beginPath();
-			ctx.rect(element.x, element.y, 5, 5);
+			ctx.rect(aug_x, aug_y, 5, 5);
 			ctx.fillStyle = "#FFFFFF";
-			
+
 			ctx.fill();
 		});
 	}
